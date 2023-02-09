111c85102c76f10a5f4c4b3abb7387b473eee61b
Trezlor <77504889+Trezlor@users.noreply.github.com>
2023-02-09 03:15:48 +0100

    pointer cursor on btn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const numberInput = document.getElementById('number_input');
+// const btnConvert = document.getElementById('btn_convert');
 
 const meters = document.getElementById('meters');
 const feet = document.getElementById('feet');
@@ -19,6 +20,8 @@ const focusInput = () => {
 };
 window.onload = focusInput();
 
+// btnConvert.onmousedown = () => focusInput();
+
 numberInput.addEventListener('keydown', (e) => {
 	if (e.code === 'Enter') {
 		convertValue();
@@ -35,17 +38,13 @@ function convertValue() {
 		valueInput = 0;
 	}
 
-	//Edits the values of the meter unit based on the input
 	meters.textContent = `${valueInput} meters = ${(valueInput * 3.28084).toFixed(2)} feet`;
 	feet.textContent = `${valueInput} feet = ${(valueInput * 0.3048).toFixed(2)} meters`;
 
-	//Edits the values of the liter unit based on the input
 	liters.textContent = `${valueInput} liters = ${(valueInput * 0.264172).toFixed(2)} gallons`;
 	gallons.textContent = `${valueInput} gallons = ${(valueInput * 3.78541).toFixed(2)} liters`;
 
-	//Edits the values of the kilo unit based on the input
 	kilos.textContent = `${valueInput} kilos = ${(valueInput * 2.20462).toFixed(2)} pounds`;
 	pounds.textContent = `${valueInput} pounds = ${(valueInput * 0.453592).toFixed(2)} kilos`;
 }
-
 convertValue();
